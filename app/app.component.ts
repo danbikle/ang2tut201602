69bf435b7859054cb1f69464a035ab1bf88af04a
Dan Bikle <dan.bikle@gmail.com>
2016-01-29 10:03:25 +0000

    mo

---
+++ app/app.component.ts
@@ -0,0 +1,21 @@
+// Ref:
+// https://angular.io/docs/ts/latest/quickstart.html
+
+// In Angular2, what does import do?
+
+import {Component} from 'angular2/core';
+
+// In Angular2, when I use the @ sign, what does the @ sign do?
+
+@Component({
+    // In Angular2, what does selector property do?
+    selector: 'my-app',
+
+    // In Angular2, what does template property do?
+    template: '<h1>My First Angular 2 App</h1>'
+})
+
+// In Angular2, what does export do?
+export class AppComponent { }
+// In Angular2, what does class do?
+// In my Angular2 app, where else do I see AppComponent?
