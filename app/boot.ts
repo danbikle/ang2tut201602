0ae18fe6c76957c4e1fa77457d92613ad6b264ee
Dan Bikle <dan.bikle@gmail.com>
2016-01-29 11:29:14 +0000

    mo

---
+++ app/boot.ts
@@ -0,0 +1,19 @@
+// boot.ts
+
+// Why is this file called boot.ts?
+
+// Ref:
+// https://angular.io/docs/ts/latest/quickstart.html#modules
+// https://angular.io/docs/ts/latest/quickstart.html#give-it-the-boot
+// In Angular2, what does import do?
+
+// What does the import statement do here?
+import {bootstrap}    from 'angular2/platform/browser'
+// What is bootstrap?
+// How do I see it?
+
+
+// What does the import statement do here?
+import {AppComponent} from './app.component'
+
+bootstrap(AppComponent);
